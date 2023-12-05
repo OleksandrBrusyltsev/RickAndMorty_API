e1bff9a9f68520eb198199bb370b418102fd588f
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-12-06 00:35:59 +0200

    refactoring and add tests

--- src/api.py
+++ src/api.py
@@ -1,61 +1,104 @@
-import os
 import requests
+import logging
 
 from data_processor import DataProcessor
 from logger import Logger
 from dotenv import load_dotenv
+from typing import Any, Dict, List, Optional
+
+from dataclass import Character, Location, Episode
+import logging.config
+import logging
+
+logging.config.fileConfig('logging.ini')
 
 load_dotenv()
 
 
 class APIService:
-    def __init__(self, base_url):
+    def __init__(self, base_url: str):
         self.base_url = base_url
 
-    def send_request(self, endpoint):
-        url = f"{self.base_url}/{endpoint}"
+    def send_request(self, url: str) -> Optional[Dict[str, Any]]:
+        """
+        Send a request to the API and retrieve the JSON response.
+        """
+
         try:
             response = requests.get(url)
             response.raise_for_status()
             return response.json()
         except requests.RequestException as e:
-            print(f"Error during request: {e}")
+            logging.exception(f"Error during request: {e}")
             return None
 
 
 class RickAndMortyAPI:
-    def __init__(self, api_service):
+    def __init__(self, api_service: APIService):
         self.api_service = api_service
 
-    def fetch_all_data(self, endpoint):
+    def fetch_obj(self, obj: str) -> List[Dict[str, Any]]:
+        """Fetch objects from the API."""
+
         results = []
-        url = endpoint
+        url = f'{self.api_service.base_url}/{obj}'
+
         while url:
             data = self.api_service.send_request(url)
             if data:
-                results.extend(data['results'])
-                url = data['info']['next']
+                results.extend(data.get('results', []))
+                url = data.get('info', {}).get('next')
             else:
                 break
+
         return results
 
 
+    def get_characters(self) -> List[Character]:
+        """Retrieve characters data from the API."""
+        character_data = self.fetch_obj('character')
+        return [Character(**char) for char in character_data]
+
+    def get_locations(self) -> List[Location]:
+        """Retrieve characters data from the API."""
+        location_data = self.fetch_obj('location')
+        return [Location(**loc) for loc in location_data]
+
+    def get_episodes(self) -> List[Episode]:
+        """Retrieve characters data from the API."""
+        episode_data = self.fetch_obj('episode')
+        return [Episode(**ep) for ep in episode_data]
+
+
 def main():
-    base_url = os.getenv("BASE_URL")
+    base_url = "https://rickandmortyapi.com/api"
     api_service = APIService(base_url)
 
     connector = RickAndMortyAPI(api_service)
 
     objects = ['character', 'location', 'episode']
+
+    mapping = {
+        'character': Character,
+        'location': Location,
+        'episode': Episode
+    }
+
     for obj in objects:
-        data = connector.fetch_all_data(obj)
-        DataProcessor.process_data(data, obj)
+        data = connector.fetch_obj(obj)
+        DataProcessor.process_data(data, obj, mapping)
 
-    episodes_data = DataProcessor.process_data(connector.fetch_all_data('episode'), 'episode')
-    locations_data = DataProcessor.process_data(connector.fetch_all_data('location'), 'location')
+    episodes_data = connector.get_episodes()
+    locations_data = connector.get_locations()
 
-    Logger.log_episodes(episodes_data)
-    Logger.log_locations(episodes_data, locations_data)
+    episodes_logger = Logger()
+    locations_logger = Logger()
+    #
+    episodes_result = episodes_logger.log_episodes_year(episodes_data)
+    logging.info("Episodes aired between 2017 and 2021 with more than three characters: %s", episodes_result)
+
+    locations_result = locations_logger.log_locations(episodes_data, locations_data)
+    logging.info("Locations that appear only on odd episode numbers: %s", locations_result)
 
 
 if __name__ == "__main__":
--- src/logger.py
+++ src/logger.py
@@ -1,29 +1,56 @@
+import logging.config
+import logging
+from typing import List, Optional
+from dateutil import parser
+from dataclass import Episode, Location
+
+logging.config.fileConfig('logging.ini')
+
+
 class Logger:
     @staticmethod
-    def log_episodes(episodes):
-        if episodes is not None:
-            filtered_episodes = filter(
-                lambda eps: "2017" <= eps.get('air_date', '')[:4] <= "2021" and len(eps.get('characters', [])) > 3,
-                episodes
-            )
-            print("Episodes between 2017 and 2021 with more than three characters:")
-            for episode in filtered_episodes:
-                print(episode['name'])
+    def log_episodes_year(episodes: Optional[List[Episode]]) -> List[str]:
+        """Logs episodes within a certain date range, with specific character count."""
+        episode_names = []
+        try:
+            if episodes:
+                filtered_episodes = filter(
+                    lambda eps: (
+                            2017 <= parser.parse(eps.air_date).year <= 2021
+                            and len(eps.characters) > 3
+                    ),
+                    episodes
+                )
+                for episode in filtered_episodes:
+                    episode_names.append(episode.name)
+        except Exception as e:
+            logging.exception(f"Error logging episodes: {e}")
+        return episode_names
+
 
     @staticmethod
-    def log_locations(episodes, locations):
-        if episodes is not None:
-            odd_episodes = {episode['id'] for episode in episodes if int(episode['episode'].split('E')[-1]) % 2 != 0}
+    def log_locations(episodes: Optional[List[Episode]], locations: Optional[List[Location]]) -> List[str]:
+        """Logs locations based on episode characteristics.
 
-            odd_episode_locations = set()
-            for location in locations:
-                if any(location['url'] in episode['characters'] for episode in episodes if
-                       episode['id'] in odd_episodes):
-                    odd_episode_locations.add(location['name'])
+        Args:
+            episodes (Optional[List[Episode]]): List of episodes.
+            locations (Optional[List[Location]]): List of locations.
 
-            print("Locations appearing only in odd-numbered episodes:")
-            for location in odd_episode_locations:
-                print(location)
+        Returns:
+            List[str]: List of location names.
+        """
+        location_names = []
+        try:
+            if episodes and locations:
+                odd_episodes = {episode.id for episode in episodes if int(episode.episode.split('E')[-1]) % 2 != 0}
 
+                odd_episode_locations = set()
+                for location in locations:
+                    if all(location.url not in episode.characters for episode in episodes if
+                           episode.id in odd_episodes):
+                        odd_episode_locations.add(location.name)
 
-
+                location_names = list(odd_episode_locations)
+        except Exception as e:
+            logging.exception(f"Error logging locations: {e}")
+        return location_names
